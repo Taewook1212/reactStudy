e4eacb29627da99c5302a76f1ca6ed367f92fce9
unknown <taewoo1212@gmail>
2024-01-18 14:05:48 +0900

    react 수업 05번째 수업중

---
+++ lecture/05-like-a-react.js
@@ -0,0 +1,9 @@
+//createElement 함수
+//Dom 노드 : HTMLElement 반환
+function createElement(type, props, ...children) {}
+// createRoot 함수
+// 객체 : { render, unmount }
+
+function createRoot(domNode /*contaniner */) {}
+
+// EUID 객체의 멤버로 내보내기
